0eed187d8805e1d15cc6cb872f9529468bdf8d1b
qweaa <2275414653@qq.com>
2017-08-17 16:45:30 +0800

    合并第二歀导航条

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ $(function(){
 function onloadHTML(){
     var domJSON = {
         header:'<div class="nav"><ul><li class="active"><a href="#">云翼信息</a></li><li><a href="#">首页</a></li><li><a href="#">课程指南</a></li><li><a href="#">学习内容</a></li><li><a href="#">教师介绍</a></li><li><a href="#">制作团队</a></li></ul></div><div class="header"></div>',
+        header2:'<div class="nav2"><ul><li>云翼科技</li><li class="active"><a href="###">首页</a></li><li><a href="###">课程指南</a></li><li><a href="###">学习内容</a></li><li><a href="###">教师介绍</a></li><li><a href="###">制作团队</a></li></ul></div><div class="header"></div>',
         footer :'<div class="footer"><div class="den"></div></div>'
     }
-    $(".container .main").before(domJSON.header).after(domJSON.footer);
+    $(".container .main").before(domJSON.header2).after(domJSON.footer);
 }
