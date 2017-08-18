78fb18edc56e131539b6527354eaba1101e180fc
SugarTiger <1029109173@qq.com>
2017-08-18 10:59:22 +0800

    ball by devafei

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
 $(function(){
     onloadHTML();
+    (function(){
+        var lock = true;
+        $('.ball').mouseover(function(e){
+            e.stopPropagation();
+            
+            $(this).stop().animate({"opacity":.9});
+        }).mouseout(function(){
+            if(lock){
+                $(this).stop().animate({"opacity":.5});
+            }
+        }).click(function(){
+            if(lock){
+                $('.b1').stop().animate({"opacity":1,"left":-60,"top":-60});
+                $('.b2').stop().animate({"opacity":1,"right":-60,"top":-60});
+                $('.b3').stop().animate({"opacity":1,"left":-60,"bottom":-60});
+                $('.b4').stop().animate({"opacity":1,"right":-60,"bottom":-60});
+            }else{
+                $('.b1').stop().animate({"opacity":0,"left":0,"top":0})
+                $('.b2').stop().animate({"opacity":0,"right":0,"top":0})
+                $('.b3').stop().animate({"opacity":0,"left":0,"bottom":0})
+                $('.b4').stop().animate({"opacity":0,"right":0,"bottom":0})
+            }
+            lock = !lock;
+        });
+        window.onscroll  = function(event){
+            var top = 577+$(this).scrollTop();
+            $('.ball').animate({'top':top},10,'swing')
+        }
+        //控制球体
+        $('.ball > div').mouseover(function(){
+            $(this).find("img").attr("src",$(this).find("img").attr("src").replace('.png','-a.png'));
+            $(this).find('p').css('color','#ffba00');
+        }).mouseout(function(){
+            $(this).find('img').attr("src",$(this).find("img").attr("src").replace('-a',''));
+            $(this).find('p').css('color','#222');
+        })
+    })();
 });
 
 function onloadHTML(){
