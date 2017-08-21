4fc2f879af3376a94711e917050aa43eb9e14298
SugarTiger <1029109173@qq.com>
2017-08-21 09:40:55 +0800

    弹窗

--- js/ch01.js
+++ js/ch01.js
@@ -28,18 +28,68 @@ $(function(){
             var top = 577+$(this).scrollTop();
             $('.ball').animate({'top':top},10,'swing')
         }
-        //控制球体
-        // $('.ball > div').mouseover(function(){
-        //     $(this).find("img").attr("src",$(this).find("img").attr("src").replace('.png','-a.png'));
-        //     $(this).find('p').css('color','#ffba00');
-        // }).mouseout(function(){
-        //     $(this).find('img').attr("src",$(this).find("img").attr("src").replace('-a',''));
-        //     $(this).find('p').css('color','#222');
-        // })
     })();
     // 添加功能到footer
     (function(){
         var footer_action = $('.footer_action').html();
         $('.footer>.action').html(footer_action);
+    })();
+    // 弹窗
+    (function(){
+        //设置笔记内容
+        var noteText = "";
+        //模拟笔记内容数据
+        var note_text = '<li><p><span>ALSDFLWEOPJMGALljkaldfjlpkpfksfslflassdjljasfljdfjwfjjgaljflasjfljl</span><span>删除</span></p><div><span>2017-8-18</span></div></li><li><p><span>ALSDFLWEOPJMGALljkaldfjlpkpfksfslflassdjljasfljdfjwfjjgaljflasjfljl</span><span>删除</span></p><div><span>2017-8-18</span></div></li>';
+        function setNote(text){
+            noteText = '<div class="note"><ul>' + text + '</ul><textarea></textarea><div><button type="button">添加</button></div></div>';
+        }
+        setNote(note_text);
+
+        //设置书签内容
+        var shuqianText = "";
+        //模拟书签内容数据
+        var shuqian_text = '<li><p><span><a href="###">第三章 第二节 XXXXXXXXX</a></span><span>删除</span></p></li>';
+        function setShuqian(text){
+            shuqianText = '<div class="shuqian"><ul>' + text + '</ul></div>';
+        }
+        setShuqian(shuqian_text);
+
+        //设置进度内容
+        var pregress = {};
+        pregressText = "";
+        var studyTime = 0.5; //模拟平台传入学习时间数据
+        pregress.lase_page = ['###','第三章 第二节 XXXXXXXXX'];
+        pregress.study = 0;
+        pregress.studyTime = studyTime > 1?studyTime:"< 1";
+        function setPregress(text){
+            pregressText = '<div class="pregress"><p>学习进度：<a href="'+ text.lase_page[0] +'">'+ text.lase_page[1] +'</a></p><p>学习完成度：'+ text.study +'%</p><p>学习时长：'+ text.studyTime +'小时</p>'
+        }
+        setPregress(pregress);
+        //存储弹窗内容
+        var popArr = {
+            note : noteText,
+            shuqian : shuqianText,
+            pregress : pregressText
+        }
+        //弹窗动画
+        $('.b1').on("click",function(){
+            $('.pop .pop-main').html(popArr.note);
+            $('.pop .pop-header h3').text("笔记");
+            $('.pop').stop().animate({"top":0});
+        })
+        $('.b2').on("click",function(){
+            $('.pop .pop-main').html(popArr.shuqian);
+            $('.pop .pop-header h3').text("书签");
+            $('.pop').stop().animate({"top":0});
+        })
+        $('.b3').on("click",function(){
+            $('.pop .pop-main').html(popArr.pregress);
+            $('.pop .pop-header h3').text("进度");
+            $('.pop').stop().animate({"top":0});
+        })
+        //关闭弹窗
+        $('.pop .pop-header span').on('click',function(){
+            $('.pop').stop().animate({"top":'-1500px'});
+        })
     })();
 });
--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ $(function(){
 });
 
 var domJSON = {
-    header:'<div class="nav"><ul><li class="active"><a href="#">云翼信息</a></li><li><a href="#">首页</a></li><li><a href="#">课程指南</a></li><li><a href="#">学习内容</a></li><li><a href="#">教师介绍</a></li><li><a href="#">制作团队</a></li></ul></div><div class="header"></div>',
+    header:'<div class="nav"><ul><li class="active"><a href="#">云翼信息</a></li><li><a href="http://127.0.0.1:8020/web36/index.html">首页</a></li><li><a href="#">课程指南</a></li><li><a href="#">学习内容</a></li><li><a href="#">教师介绍</a></li><li><a href="#">制作团队</a></li></ul></div><div class="header"></div>',
     footer :'<div class="footer"><div class="den"></div><ul class="action"></ul></div>',
-    ball:'<div class="ball"><div class="b1"><img src="../../images/note.png" alt="笔记"><p>笔记</p></div><div class="b2"><img src="../../images/shuqian.png" alt="书签"><p>书签</p></div><div class="b3"><img src="../../images/pregress.png" alt="进度"><p>进度</p></div><div class="b4"><img src="../../images/work.png" alt="作业"><p>作业</p></div></div>'
+    ball:'<div class="ball"><div class="b1"><img src="../../images/note.png" alt="笔记"><p>笔记</p></div><div class="b2"><img src="../../images/shuqian.png" alt="书签"><p>书签</p></div><div class="b3"><img src="../../images/pregress.png" alt="进度"><p>进度</p></div><div class="b4"><img src="../../images/work.png" alt="作业"><p>作业</p></div></div>',
+    pop:'<div class="pop"><div class="pop-header"><h3>笔记</h3><span>x</span></div><div class="pop-main"></div></div>'
 }
 var chapterArr = {
-    c01 : [domJSON.ball]
+    c01 : [domJSON.ball,domJSON.pop]
 }
 
 function load(arr){
